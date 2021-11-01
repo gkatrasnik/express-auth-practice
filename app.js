01faa2a224259bb789fcac5857c28507ece5bfab
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-11-01 21:53:28 +0100

    env mongo_uri changed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
-const mongoDb = process.env.MONGO_URL;
+const mongoDb = process.env.MONGODB_URI;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
